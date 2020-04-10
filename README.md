d7110aa8ecd809a2fbf18f4a629e26be12b977ff
Pankaj <Pankaj.dwivedi@valvoline.com>
2020-04-11 02:03:36 +0530

    Portfolio

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio 
+Portfolio template
+## Credits
+Template was created by Tenzin Phuljung and edited by Pankaj Dwivedi.
---
+++ assets/scripts/app.js
@@ -0,0 +1,84 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "T-SQL",
+				weight: 12.3
+			}, {
+				text: "PL/SQL",
+				weight: 8
+			}, {
+				text: "python",
+				weight: 14
+			}, {
+				text: "jquery",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "javascript",
+				weight: 10
+			}, {
+				text: "java",
+				weight: 9
+			}, {
+				text: "Tuning",
+				weight: 15
+			}, {
+				text: "DBA",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Database Developer and DBA.", "make SQL server go faster.", "solution provider.", "love data."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
